5d159ee46036cd0c243306714bc0524b3ff643ad
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2018-09-30 13:19:09 +0200

    add module for spring security service

---
+++ spring/spring-security-service/src/main/java/io/neko/springsecurityservice/SpringSecurityService.java
@@ -0,0 +1,12 @@
+package io.neko.springsecurityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityService.class, args);
+    }
+}
---
+++ spring/spring-security-service/src/test/java/io/neko/springsecurityservice/SpringSecurityServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package io.neko.springsecurityservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSecurityServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
