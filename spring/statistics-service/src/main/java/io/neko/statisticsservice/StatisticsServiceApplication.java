617475473b941def72e618791e8d14bc0fbe4cbb
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2018-11-04 11:54:42 +0100

    initila commit for project structure

---
+++ spring/statistics-service/src/main/java/io/neko/statisticsservice/StatisticsServiceApplication.java
@@ -0,0 +1,12 @@
+package io.neko.statisticsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatisticsServiceApplication.class, args);
+    }
+}
